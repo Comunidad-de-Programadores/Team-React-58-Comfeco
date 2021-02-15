e103ae59ae748182ce90c8a8bd234f9bc173ee2d
armandojes <armandodejesus@gmail.com>
2021-02-14 20:57:14 -0600

    render

--- source/app/index.jsx
+++ source/app/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Header from 'app/components/header/styles';
-import Login from './pages/Login/Login';
+import { Switch, Route } from 'react-router';
+import Register from './pages/register';
+import Login from './pages/login';
+
 import './style.css';
 import Footer from './components/footer';
 
 const App = () => (
   <div>
     <Header />
-    <Login />
+    <Switch>
+      <Route path="/login" exact component={Login} />
+      <Route path="/register" exact component={Register} />
+    </Switch>
     <Footer />
   </div>
 
