6ed3e1344faae61bcda27475e024e6dbc090fbc1
armandojes <armandodejesus@gmail.com>
2021-02-14 18:18:17 -0600

    add Input controlled at components

--- source/app/components/input/index.jsx
+++ source/app/components/input/index.jsx
@@ -1,27 +1,33 @@
 /* eslint-disable react/destructuring-assignment */
-import { string, array } from 'prop-types';
+import { string, array, object } from 'prop-types';
 
 import React from 'react';
 
 const Input = ({ name, values, inputWithErrors, ...otherProps }) => {
   const value = values[name];
-  const error = inputWithErrors[name];
+  const error = inputWithErrors.includes(name);
   const styleForError = { borderColor: 'red' };
 
   return (
     <input
       {...otherProps}
       name={name}
-      value={value}
+      value={value || ''}
       style={error ? styleForError : {}}
     />
   );
 };
 
+Input.defaultProps = {
+  values: [],
+  name: '',
+  inputWithErrors: [],
+};
+
 Input.propTypes = {
-  values: array.isRequired,
-  name: string.isRequired,
-  inputWithErrors: array.isRequired,
+  values: object,
+  name: string,
+  inputWithErrors: array,
 };
 
 export default Input;
