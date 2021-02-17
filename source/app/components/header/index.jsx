bf38132f1c3ab23225c6e30560ae6f326a8e312a
Philip Perez Castro <jperez.c92@gmail.com>
2021-02-17 02:22:21 -0500

    añadida condicion para mostrar boton 'iniciar sesion' en header

--- source/app/components/header/index.jsx
+++ source/app/components/header/index.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import isotipo from '../../images/Isotipo-brand.png';
 import logo1 from '../../images/logo1.png';
 import Button from './Button';
 import styles from './styles.css';
 
-const Header = () => (
-  <>
-    <header className={styles.mainHeader}>
-      <picture className={styles.mainHeader__logo}>
-        <source media="(min-width: 768px)" srcSet={logo1} sizes="" />
-        <img src={isotipo} alt="" srcSet="" />
-      </picture>
+const Header = () => {
+  const location = useLocation().pathname;
+  const locationToShow = '/recoverAccount';
 
-      <div className={styles.mainHeader__buttons}>
-        <Link to="/login">
-          <Button type="button">Iniciar Sesión</Button>
-        </Link>
-      </div>
+  return (
+    <>
+      <header className={styles.mainHeader}>
+        <picture className={styles.mainHeader__logo}>
+          <source media="(min-width: 768px)" srcSet={logo1} />
+          <img src={isotipo} alt="comfeco" />
+        </picture>
 
-    </header>
-    <hr className={styles.mainHeader__underline} />
-  </>
-);
+        <div className={styles.mainHeader__buttons}>
+          {location === locationToShow && (
+          <Link to="/login">
+            <Button type="button">Iniciar Sesión</Button>
+          </Link>
+          )}
+        </div>
+
+      </header>
+      <hr className={styles.mainHeader__underline} />
+    </>
+  );
+};
 
 export default Header;
