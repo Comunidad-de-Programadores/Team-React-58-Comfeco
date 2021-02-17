e685fe011295469559ee8201913312d2d1880b7b
yenselleon <76271225+yenselleon@users.noreply.github.com>
2021-02-16 22:24:26 -0400

    animacion del formulario agregada

--- source/app/pages/auth/index.jsx
+++ source/app/pages/auth/index.jsx
@@ -1,59 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SignUp from '../../components/signUp/index';
 import styles from './style.css';
-import poligono from '../../images/poligonoAsset.svg'
+import poligono from '../../images/poligonoAsset.svg';
 import loginImg from '../../images/login2.svg';
 import signInImg from '../../images/signIn.svg';
 import SignIn from '../../components/signIn/index';
 
-const Auth = () => (
-  <div className={styles.wrapper}>
+const Auth = () => {
 
-    {/* container form */}
-    <div className={styles.cotainer__Form}>
+  const [addClass, setAddClass] = useState(false);
 
-      {/*------------------------------------------*/
+  const handleAddClass = () => {
+
+    setAddClass(!addClass);
+
+  };
+
+  return (
+
+    <div className={styles.wrapper}>
+      
+      {/* container form */}
+      <div className={ `${styles.cotainer__Form} ${addClass ? styles.signUp__mode : ''}` }>
+
+        <p className={styles.paragraph__button} >¿No tienes cuenta?</p>
+        <button className={styles.button__submit} type="submit" onClick={handleAddClass} >Crear Una Cuenta</button>
+
+        <p className={styles.paragraph__button__submit__signUp__mode} >¿Ya tienes una cuenta?</p>
+        <button className={styles.button__submit__signUp__mode} type="submit" onClick={handleAddClass} >Iniciar Sesión</button>
+
+        <img className={styles.loginImg} src={loginImg} alt="login" />
+        <img className={styles.signInImg} src={signInImg} alt="login" />
+        <img className={styles.wrapper__poligono} src={poligono} alt="poligono" />
+
+        {/*------------------------------------------*/
       /*  -- Form model sing Up --  */
       /*------------------------------------------*/}
-      <div className={styles.wrapper__form__signUp}>
+        <div className={styles.wrapper__form__signUp}>
 
-        <img className={styles.form__poligono} src={poligono} alt="poligono"/>
+          {/* Section Poligono */}
+          <div className={styles.polygonSection}>
+            {/* <img className={styles.loginImg} src={loginImg} alt="login" /> */}
+          </div>
+          {/* section form */}
+          <div className={styles.form__Section}>
+            <SignUp />
+          </div>
 
-        {/* Section Poligono */}
-        <div className={styles.polygonSection}>
-          <img className={styles.loginImg} src={loginImg} alt="login"/>
         </div>
-        {/* section form */}
-        <div className={styles.form__Section}>
-          <SignUp />
-        </div>
 
-      </div>
-
-      {/*------------------------------------------*/
+        {/*------------------------------------------*/
       /*  -- Form model sing In --  */
       /*------------------------------------------*/}
-      <div className={styles.wrapper__form__signIn}>
+        <div className={styles.wrapper__form__signIn}>
 
-        <img className={styles.form__poligono} src={poligono} alt="poligono"/>
+          {/* section form */}
+          <div className={styles.form__Section}>
+            <SignIn />
+          </div>
 
-        {/* section form */}
-        <div className={styles.form__Section}>
-          <SignIn />
-        </div>
+          {/* Section Poligono */}
+          <div className={styles.polygonSection}>
+            
+          </div>
 
-        {/* Section Poligono */}
-        <div className={styles.polygonSection}>
-          <img className={styles.signInImg} src={signInImg} alt="login"/>
         </div>
 
       </div>
 
+      {/* final container form */}
+
     </div>
-
-    {/* final container form */}
-
-  </div>
-);
-
+  );
+};
 export default Auth;
