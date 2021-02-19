b0be3e492a3c9e1cfed93bcf64688d60b090b5a2
yenselleon <76271225+yenselleon@users.noreply.github.com>
2021-02-18 22:37:06 -0400

    actualizacion de diseño de elementos

--- source/app/pages/auth/index.jsx
+++ source/app/pages/auth/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import SignUp from '../../components/signUp/index';
 import styles from './style.css';
-import poligono from '../../images/poligonoAsset.svg';
+import poligono from '../../images/poligonoForm.svg';
 import loginImg from '../../images/login2.svg';
 import signInImg from '../../images/signIn.svg';
 import SignIn from '../../components/signIn/index';
@@ -19,15 +19,25 @@ const Auth = () => {
   return (
 
     <div className={styles.wrapper}>
-      
+
       {/* container form */}
       <div className={ `${styles.cotainer__Form} ${addClass ? styles.signUp__mode : ''}` }>
 
-        <p className={styles.paragraph__button} >¿No tienes cuenta?</p>
-        <button className={styles.button__submit} type="submit" onClick={handleAddClass} >Crear Una Cuenta</button>
+        {/* boton y parrafo del poligono svg en el form */}
+        <div className={styles.wrapper__buttonPolygon__loginMode} >
 
-        <p className={styles.paragraph__button__submit__signUp__mode} >¿Ya tienes una cuenta?</p>
-        <button className={styles.button__submit__signUp__mode} type="submit" onClick={handleAddClass} >Iniciar Sesión</button>
+          <p className={styles.paragraph__button} >¿No tienes cuenta?</p>
+          <button className={styles.button__submit} type="submit" onClick={handleAddClass} >Crear Una Cuenta</button>
+
+        </div>
+
+        <div className={styles.wrapper__buttonPolygon__signInMode} >
+
+          <p className={styles.paragraph__button__submit__signIn__mode} >¿Ya tienes una cuenta?</p>
+          <button className={styles.button__submit__signIn__mode} type="submit" onClick={handleAddClass} >Iniciar Sesión</button>
+
+        </div>
+
 
         <img className={styles.loginImg} src={loginImg} alt="login" />
         <img className={styles.signInImg} src={signInImg} alt="login" />
@@ -38,23 +48,14 @@ const Auth = () => {
       /*------------------------------------------*/}
         <div className={styles.wrapper__form__signUp}>
 
-<<<<<<< HEAD
           {/* Section Poligono */}
-          <div className={styles.polygonSection}>
-            {/* <img className={styles.loginImg} src={loginImg} alt="login" /> */}
-          </div>
+          <div className={styles.polygonSection} />
+
           {/* section form */}
           <div className={styles.form__Section}>
             <SignUp />
           </div>
 
-=======
-        <img className={styles.form__poligono} src={poligono} alt="poligono" />
-
-        {/* Section Poligono */}
-        <div className={styles.polygonSection}>
-          <img className={styles.loginImg} src={loginImg} alt="login" />
->>>>>>> 7af91afea923f15ea2752e285d02cd5ba8735037
         </div>
 
         {/*------------------------------------------*/
@@ -62,26 +63,14 @@ const Auth = () => {
       /*------------------------------------------*/}
         <div className={styles.wrapper__form__signIn}>
 
-<<<<<<< HEAD
           {/* section form */}
           <div className={styles.form__Section}>
             <SignIn />
           </div>
-=======
-        <img className={styles.form__poligono} src={poligono} alt="poligono" />
->>>>>>> 7af91afea923f15ea2752e285d02cd5ba8735037
 
           {/* Section Poligono */}
-          <div className={styles.polygonSection}>
-            
-          </div>
+          <div className={styles.polygonSection} />
 
-<<<<<<< HEAD
-=======
-        {/* Section Poligono */}
-        <div className={styles.polygonSection}>
-          <img className={styles.signInImg} src={signInImg} alt="login" />
->>>>>>> 7af91afea923f15ea2752e285d02cd5ba8735037
         </div>
 
       </div>
