283d7d8de958d5e293f310321c74a5842d399e34
yenselleon <76271225+yenselleon@users.noreply.github.com>
2021-02-20 00:20:49 -0400

    inicio del componente carousel

--- source/app/pages/landing/index.jsx
+++ source/app/pages/landing/index.jsx
@@ -1,9 +1,18 @@
+import Carousel from 'app/components/carousel';
 import React from 'react';
 
 const Landing = () => (
-  <h1 style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '80vh' }}>
-    Bienvenido a fest code
-  </h1>
+  <>
+    <h1 style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '10vh' }}>
+      Bienvenido a fest code
+    </h1>
+
+    <div style={{ width: '50%' }}>
+
+      <Carousel />
+
+    </div>
+  </>
 );
 
 export default Landing;
