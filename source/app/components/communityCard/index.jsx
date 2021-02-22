2666bebb776251dc70b04830fc6fc0a13ba61fa6
Philip Perez Castro <jperez.c92@gmail.com>
2021-02-21 23:18:42 -0500

    añadidos communityCard & workshopCard

---
+++ source/app/components/communityCard/index.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styles from './styles.css';
+import logo from '../../images/Isotipo-brand.png';
+
+const index = () => (
+  <div className={styles.community}>
+    <header className={styles.community__header}>
+      <h3 className={styles.community__tittle}>Communidades</h3>
+      <a className={styles.community__button} href="/">
+        Ver más
+      </a>
+    </header>
+
+    <ul className={styles.community__grid}>
+      <li className={styles.community__item}>
+        <div className={styles.communityItem__logo}>
+          <img src={logo} alt="communityLogo" />
+        </div>
+        <h4 className={styles.communityItem__title}>Comunidad de Programadores</h4>
+
+        <a className={styles.community__button} href="/">
+          Unirme
+        </a>
+      </li>
+      <hr />
+
+      <li className={styles.community__item}>
+        <div className={styles.communityItem__logo}>
+          <img src={logo} alt="communityLogo" />
+        </div>
+        <h4 className={styles.communityItem__title}>El lenguaje de los programadores</h4>
+
+        <a className={styles.community__button} href="/">
+          Unirme
+        </a>
+      </li>
+      <hr />
+
+      <li className={styles.community__item}>
+        <div className={styles.communityItem__logo}>
+          <img src={logo} alt="communityLogo" />
+        </div>
+        <h4 className={styles.communityItem__title}>Latam Dev</h4>
+
+        <a className={styles.community__button} href="/">
+          Unirme
+        </a>
+      </li>
+      <hr />
+    </ul>
+  </div>
+);
+
+export default index;
