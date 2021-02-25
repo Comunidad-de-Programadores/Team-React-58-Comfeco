de7b34c4f4b6e3663b450831c0fb0793f400683a
armandojes <armandodejesus@gmail.com>
2021-02-24 20:54:14 -0600

    update landing

--- source/app/pages/landing/index.jsx
+++ source/app/pages/landing/index.jsx
@@ -1,18 +1,27 @@
+import { Box } from '@material-ui/core';
 import CarouselCreadores from 'app/components/carousel/index-creadores';
 import CarouselSponsors from 'app/components/carousel/index-sponsors';
+import CommunitiesCard from 'app/components/communityCard'
 import React from 'react';
 import styles from './styles.css';
 
 const Landing = () => (
-  <div className={styles.wrapper}>
-    <div className={styles.column}>
-      communities Card
+  <>
+    <h1 className={styles.title}>Bienvenido a Comunidad Fest</h1>
+    <div className={styles.wrapper}>
+      <div className={styles.column}>
+        <CommunitiesCard />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.carrouselWrapper}>
+          <CarouselCreadores />
+        </div>
+        <div className={styles.carrouselWrapper}>
+          <CarouselSponsors />
+        </div>
+      </div>
     </div>
-    <div className={styles.content}>
-      <CarouselCreadores />
-      <CarouselSponsors />
-    </div>
-  </div>
+  </>
 );
 
 export default Landing;
