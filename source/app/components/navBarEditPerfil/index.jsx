add660b655a787f8dee43a0e9e3604b62272b2ed
yenselleon <76271225+yenselleon@users.noreply.github.com>
2021-03-05 22:48:36 -0400

    nuevo componente creado

--- source/app/components/navBarEditPerfil/index.jsx
+++ source/app/components/navBarEditPerfil/index.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import styles from './styles.css'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './styles.css';
 
-const NavBarEditPerfil = () => {
-    return (
-        <div className={styles.wrapper__container}>
-            <div className={styles.container}>
-                <button>Mi perfil</button>
-                <button>Insignias</button>
-                <button>Grupos</button>
-                <button>Eventos</button>
-            </div>
-        </div>
-    )
-}
+const NavBarEditPerfil = () => (
+  <div className={styles.wrapper__container}>
+    <div className={styles.container}>
+      <button>Mi perfil</button>
+      <button>Insignias</button>
+      <button>Grupos</button>
+      <button>Eventos</button>
+    </div>
+  </div>
+);
 
 export default NavBarEditPerfil;
