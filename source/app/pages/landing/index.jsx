4fa8f473e009d6780ac185299c622c195397aaf9
armandojes <armandodejesus@gmail.com>
2021-03-10 16:31:02 -0600

    updated

--- source/app/pages/landing/index.jsx
+++ source/app/pages/landing/index.jsx
@@ -5,17 +5,13 @@ import CommunitiesCard from 'app/components/communityCard';
 import Countdown from '../../components/countdown';
 import styles from './styles.css';
 
-
 const Landing = () => (
   <div className={styles.wrapper}>
-
     {/* header */}
     <div className={styles.svg__header}>
-
       {/* <img src={semiCircleSvg} alt=""/> */}
 
       <div className={styles.header__content}>
-
         <h1>Bienvenido a Fest And Code</h1>
         <h3>Conoce gente Aprende y Gana</h3>
         <h4>Ellos son nuestros colaboradores:</h4>
@@ -24,21 +20,17 @@ const Landing = () => (
       <div className={styles.carrouselCreadoresWrapper}>
         <CarouselCreadores />
       </div>
-
     </div>
 
     {/* content */}
     <div className={styles.wrapper__content}>
-
       <div className={styles.content}>
-
         {/* side */}
         <div className={styles.column}>
           <CommunitiesCard />
         </div>
 
         {/* center */}
-
 
         <div className={styles.wrapper__center}>
           <h4 className={styles.title__center}>Nuestros Sponsors</h4>
@@ -50,16 +42,10 @@ const Landing = () => (
           </div>
         </div>
 
-
         {/* side */}
-        <div className={styles.column}>
-          {/* <CommunitiesCard /> */}
-        </div>
-
+        <div className={styles.column}>{/* <CommunitiesCard /> */}</div>
       </div>
-
     </div>
-
   </div>
 );
 
