1398e85a2f89c4ce8cb54fdc566e7f1a1a8c6905
armandojes <armandodejesus@gmail.com>
2021-03-10 13:42:32 -0600

    update

--- source/app/pages/myAccount/components/eventCard/index.jsx
+++ source/app/pages/myAccount/components/eventCard/index.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable prettier/prettier */
 import { ButtonBase } from '@material-ui/core';
-import { string } from 'prop-types';
+import { string, instanceOf } from 'prop-types';
 import React from 'react';
 import limitText from '../../../../helpers/limitText';
 import styles from './styles.css';
 
-const Eventcard = ({ image, name, description }) => (
+const Eventcard = ({ image, name, description, date }) => (
   <div className={styles.wrapper}>
     <img src={image} className={styles.image} alt="event cover" />
     <div className={styles.dataWrapper}>
       <div className={styles.title}>{name}</div>
+      <div className={styles.date}>{new Date(date).toLocaleDateString()}</div>
       <div className={styles.description}>{limitText(description, 200)}</div>
       <div className={styles.actionWrapper}>
         <ButtonBase className={styles.button}>Mas información</ButtonBase>
@@ -22,7 +23,8 @@ const Eventcard = ({ image, name, description }) => (
 Eventcard.propTypes = {
   image: string.isRequired,
   name: string.isRequired,
-  description: string.isRequired
+  description: string.isRequired,
+  date: instanceOf(Date).isRequired
 }
 
 export default Eventcard;
