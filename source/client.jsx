7437b8252fa78b0e0c23033c883bf6b0d37e6f2b
armandojes <armandodejesus@gmail.com>
2021-03-16 13:34:38 -0600

    update

--- source/client.jsx
+++ source/client.jsx
@@ -4,12 +4,6 @@ import { hydrate } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'app/index';
 import './app/style.css';
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-
-if (process.env.NODE_ENV === 'production') {
-  disableReactDevTools();
-}
-console.log(process.env.NODE_ENV);
 
 const Client = () => (
   <BrowserRouter>
