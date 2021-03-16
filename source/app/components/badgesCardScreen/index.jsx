86a10cc6ca8b37e206235e0308fd90faa061e6c4
yenselleon <76271225+yenselleon@users.noreply.github.com>
2021-03-15 22:57:20 -0400

    cambios a la card

--- source/app/components/badgesCardScreen/index.jsx
+++ source/app/components/badgesCardScreen/index.jsx
@@ -2,49 +2,36 @@ import React from 'react';
 import useBadges from 'app/hooks/useBadges';
 import styles from './styles.css';
 
-
 const BadgesCardScreen = () => {
   const { isLoading, badges } = useBadges();
-
 
   return (
     <div className={styles.badges__main__wrapper}>
       <div className={styles.badges__card__wrapper}>
+        {isLoading ? (
+          <h1 className={styles.card__loading}>Cargando</h1>
+        ) : (
+          badges.map((badge) => (
+            <div key={badge.id} className={styles.badges__Card}>
+              <div className={styles.badges__circle__wrapper}>
+                <div className={styles.middle__wrapper__circle} />
+                <div className={styles.badge__circle}>
+                  <img src={badge.image} alt="" />
+                </div>
+              </div>
 
-        {
-          (isLoading)
-          ? <h1 className={styles.card__loading}>Cargando</h1>
-          : badges.map(badge => (
-            
-        
-        <div key={ badge.id } className={styles.badges__Card}>
-          
-          <div className={styles.badges__circle__wrapper}>
-            <div className={styles.middle__wrapper__circle} />
-            <div className={styles.badge__circle}>
-              <img src={badge.image} alt="" />
+              <div className={styles.badges__body__Card}>
+                <span>{badge.name}</span>
+                <p>{badge.description}</p>
+
+                <hr />
+
+                <span>¿Como Ganarla?</span>
+                <p>{badge.howToGetIt}</p>
+              </div>
             </div>
-          </div>
-
-          
-          <div className={styles.badges__body__Card}>
-            <span>{badge.name}</span>
-            <p>{badge.description}</p>
-
-            <hr />
-
-            <span>¿Como Ganarla?</span>
-            <p>
-              {badge.howToGetIt}
-            </p>
-          </div>
-        </div>
-          )) 
-
-        }
-        
-
-
+          ))
+        )}
       </div>
     </div>
   );
