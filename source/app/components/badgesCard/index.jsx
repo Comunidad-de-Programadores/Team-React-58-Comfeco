bbf44b794c7ac85b559d007a020be5cddd3a1ff4
Philip Perez Castro <jperez.c92@gmail.com>
2021-03-17 22:08:45 -0500

    modificado componente badgesCard, ahora muestra el nombre de la insignia

--- source/app/components/badgesCard/index.jsx
+++ source/app/components/badgesCard/index.jsx
@@ -12,13 +12,16 @@ const BadgesCard = () => {
       <h3 className={styles.badgesCard__title}>Insignias</h3>
       <div className={styles.badgesCard__wrapper}>
         {userBadges.map((badge) => (
-          <img
-            key={badge._id}
-            className={styles.badgesCard__logo}
-            src={badge.image}
-            alt={`${badge.name} badge`}
-            title={badge.description}
-          />
+          <span className={styles.badgesCard__item}>
+            <img
+              key={badge._id}
+              className={styles.badgesCard__logo}
+              src={badge.image}
+              alt={`${badge.name} badge`}
+              title={badge.description}
+            />
+            <span>{badge.name}</span>
+          </span>
         ))}
       </div>
     </div>
