61bb432af3e2bdee07bfe113ce89df5d0425f0d5
yenselleon <76271225+yenselleon@users.noreply.github.com>
2021-03-17 23:56:24 -0400

    cambios al componente editPerfil

---
+++ source/app/helpers/knowledges.js
@@ -0,0 +1,11 @@
+const knowledges = [
+  'Frontend',
+  'Backend',
+  'DevOps',
+  'Video Game Developer',
+  'UI/UX',
+  'Data Base Developer',
+  'Cloud Computing',
+];
+
+export default knowledges;
